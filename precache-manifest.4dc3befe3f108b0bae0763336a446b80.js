d1a413991197d737c7026809ca562f764f3cc9ce
GitHub Action <anonymous@example.com>
2018-12-19 13:39:07 +0000

    Updates

---
+++ precache-manifest.4dc3befe3f108b0bae0763336a446b80.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "85f2d7cf02eb58a5c875",
+    "url": "/react-marquee/static/js/runtime~main.85f2d7cf.js"
+  },
+  {
+    "revision": "a13b36be21ab924bcf52",
+    "url": "/react-marquee/static/js/main.a13b36be.chunk.js"
+  },
+  {
+    "revision": "02244194e1291b735590",
+    "url": "/react-marquee/static/js/1.02244194.chunk.js"
+  },
+  {
+    "revision": "37a0c63c48190350385cf1be7096132e",
+    "url": "/react-marquee/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7460b5bcdf27449900ba06754db41d9a.js"
+  "/react-marquee/precache-manifest.4dc3befe3f108b0bae0763336a446b80.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-marquee/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
